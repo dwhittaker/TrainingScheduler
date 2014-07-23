48ecce9c536521d319a371e7a8eed26998ee2056
dwhittaker <dwhittaker13@yahoo.com>
2014-07-23 11:47:12 -0400

    Delete Completed.sql

--- UploadTest/Imports/Imports/Completed.sql
+++
@@ -1,6 +0,0 @@
-select * from modules where moduleid in (select distinct moduleid from employeemodule)
-
-
-select * from employee where emp_id in (select distinct emp_id from empregistration where completiondate is not null and EmpModID in (select empmodid from employeemodule where moduleid in (18,19,20,21,23,26,27,28,29))
-and completiondate between '1/1/2014' and '1/31/2014')
-order by last_name
